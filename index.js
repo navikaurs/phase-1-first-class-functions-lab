8f952aa15afbae6adc8c0ed3f52c888ab428e21d
Navi Kaur <navdeep9520@gmail.com>
2021-09-22 14:19:43 -0700

    commit tree 9da5ca9da6ced7508859257160e518898c7e9584

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayDrivers) {
+    return arrayDrivers.slice (0,2)
+}
+const returnLastTwoDrivers = function (arrayDrivers) {
+    return arrayDrivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+console.log(selectingDrivers);
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+   
+function selectDifferentDrivers(arrayOfDrivers, callbackFunction){
+// console.log(arrayOfDrivers, "array of drivers");
+// console.log(callbackFunction, "callback function");
+return callbackFunction(arrayOfDrivers);
+}
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia' ];
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
